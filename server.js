d172efdb5de2f6a4ceb28d2afbcfa7f11f74306c
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-04-23 15:51:01 +0200

    import body-parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import bodyParser from 'body-parser';
 import mongoose from "mongoose";
 import {makeExcuteableSchema} from "graphql-tools";
 import webpack from "webpack";
